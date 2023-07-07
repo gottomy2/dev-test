bb83cb73882a540e621c4dc3077c8e8545817174
gottomy2 <igottomy@gmail.com>
2023-07-07 11:12:46 +0200

    update: added UnionFind class to deal with finding connected components in a graph

--- src/ConnectedGraphsFinder.java
+++ src/ConnectedGraphsFinder.java
@@ -1,2 +1,45 @@
 public class ConnectedGraphsFinder {
+    public class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int size) {
+            parent = new int[size];
+            rank = new int[size];
+
+            // Initially, each element is its own parent.
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int element) {
+            if (parent[element] != element) {
+                // Path compression
+                parent[element] = find(parent[element]);
+            }
+            return parent[element];
+        }
+
+        public void union(int element1, int element2) {
+            int root1 = find(element1);
+            int root2 = find(element2);
+
+            // Elements are already in the same set.
+            if (root1 == root2) {
+                return;
+            }
+
+            // Union by rank
+            if (rank[root1] < rank[root2]) {
+                parent[root1] = root2;
+            } else if (rank[root1] > rank[root2]) {
+                parent[root2] = root1;
+            } else {
+                parent[root2] = root1;
+                rank[root1]++;
+            }
+        }
+    }
 }
