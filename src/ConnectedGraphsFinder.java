48d8e72a82559d0e33dd6d37bb1525fed3b044c6
gottomy2 <igottomy@gmail.com>
2023-07-25 15:21:46 +0200

    update: replaced Lists with HashMaps to provide more reliable solution - Hashmaps can accept any integer as a key

--- src/ConnectedGraphsFinder.java
+++ src/ConnectedGraphsFinder.java
@@ -17,26 +17,20 @@ public class ConnectedGraphsFinder {
      * that deal with finding connected components in a graph.
      */
     public static class UnionFind {
-        private List<Integer> parent;
-        private List<Integer> rank;
+        private Map<Integer, Integer> parent;
+        private Map<Integer, Integer> rank;
 
         /**
          * Initializes a UnionFind data structure for the given size.
          *
-         * @param size The total number of elements.
+         * parent The total number of elements.
          */
-        public UnionFind(int size) {
-            parent = new ArrayList<>(Collections.nCopies(size, 0));
-            rank = new ArrayList<>(Collections.nCopies(size, 0));
-
-            // Initially, each element is its own parent.
-            for (int i = 0; i < size; i++) {
-                parent.set(i, i);
-                rank.set(i, 1);
-            }
+        public UnionFind() {
+            parent = new HashMap<>();
+            rank = new HashMap<>();
         }
 
-        /**
+        /***
          * Finds the representative element (root) of the set containing the given element.
          * Performs path compression to flatten the tree for faster subsequent finds.
          *
@@ -44,9 +38,14 @@ public class ConnectedGraphsFinder {
          * @return The representative element of the set containing the given element.
          */
         public int find(int element) {
+            if (!parent.containsKey(element)) {
+                parent.put(element, element);
+                rank.put(element, 1);
+                return element;
+            }
+
             if (parent.get(element) != element) {
-                // Path compression
-                parent.set(element, find(parent.get(element)));
+                parent.put(element, find(parent.get(element)));
             }
             return parent.get(element);
         }
@@ -63,19 +62,17 @@ public class ConnectedGraphsFinder {
             int root1 = find(element1);
             int root2 = find(element2);
 
-            // Elements are already in the same set.
             if (root1 == root2) {
                 return;
             }
 
-            // Union by rank
             if (rank.get(root1) < rank.get(root2)) {
-                parent.set(root1, root2);
+                parent.put(root1, root2);
             } else if (rank.get(root1) > rank.get(root2)) {
-                parent.set(root2, root1);
+                parent.put(root2, root1);
             } else {
-                parent.set(root2, root1);
-                rank.set(root1, rank.get(root1) + 1);
+                parent.put(root2, root1);
+                rank.put(root1, rank.get(root1) + 1);
             }
         }
     }
@@ -91,12 +88,7 @@ public class ConnectedGraphsFinder {
         System.out.println("Please enter the number of vertices:");
         int n = Integer.parseInt(scanner.nextLine());
 
-        // Using Hashmap to map vertex values to indices in the UnionFind data structure:
-        Map<Integer, Integer> vertexToIndex = new HashMap<>();
-        int index = 0;
-
-        // Using UnionFind to keep track of the connected components:
-        UnionFind uf = new UnionFind(n*2); // at most 2n vertices
+        UnionFind uf = new UnionFind();
 
         // Reading the edges and union the vertices:
         System.out.println("Please enter the edges (each edge on a separate line) a b:");
@@ -106,23 +98,14 @@ public class ConnectedGraphsFinder {
             int a = Integer.parseInt(edge.get(0));
             int b = Integer.parseInt(edge.get(1));
 
-            if(!vertexToIndex.containsKey(a)) {
-                vertexToIndex.put(a, index++);
-            }
-            if(!vertexToIndex.containsKey(b)) {
-                vertexToIndex.put(b, index++);
-            }
-
-            uf.union(vertexToIndex.get(a), vertexToIndex.get(b));
+            uf.union(a, b);
         }
 
-        // Counting the number of distinct connected components:
         Set<Integer> roots = new HashSet<>();
-        for (int i = 0; i < index; i++) {
+        for (int i : uf.parent.keySet()) {
             roots.add(uf.find(i));
         }
 
-        // Output the number of separate connected components:
         System.out.println("The number of separate connected components is: " + roots.size());
     }
 }
